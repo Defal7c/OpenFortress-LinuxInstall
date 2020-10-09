87c7b8ec82890b8d6b38a71cd2b6cba668994340
Defal7 <mth.fe.a@protonmail.com>
2020-10-09 13:01:25 -0300

    of-install

--- of-install.sh
+++ of-install.sh
@@ -20,6 +20,5 @@ then
 fi
 
 echo -e "The selected directory is: \e[34m$STEAMDIR/steam/steamapps/sourcemods\e[0m"
-cp install-tf2-sdk.txt $STEAMDIR/steam/
-
+steamcmd login anonymous force_install_dir $STEAMDIR/steam/steamapss/common/tf2/ app_update 232250
 svn co "https://svn.openfortress.fun/svn/open_fortress" "$STEAMDIR/steam/steamapps/sourcemods/open_fortress"
